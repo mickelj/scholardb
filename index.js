c47c0de1c2d33ff9bc9293342de3d3331347fc64
Jason T. Mickel <mickelj@wlu.edu>
2017-04-20 13:11:29 -0400

    Fixing errors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const app = express();
-const bodyparser = require('bodyparser');
-const index = require('./routes/index');
 const express = require('express');
+const app = express();
+
+const bodyparser = require('body-parser');
+const index = require('./routes/index');
 const massive = require('massive');
 const connectionString = "postgres://knyadlimluugnb:ab832b8ca2651471a862e489e4f44f6754019e8c795a20a582e2f64a8ec13c7d@ec2-54-225-240-168.compute-1.amazonaws.com:5432/ddk942mhci1bsc";
 const massiveInstance = massive.connectSync({connectionString: connectionString});
