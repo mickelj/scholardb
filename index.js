dae71d55fc9f1b250d837983f7d1aae71cb5244f
Jason T. Mickel <mickelj@wlu.edu>
2017-04-20 13:01:05 -0400

    Correcting launch error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const app = express();
 const bodyparser = require('bodyparser');
 const index = require('./routes/index');
 const express = require('express');
 const massive = require('massive');
 const connectionString = "postgres://knyadlimluugnb:ab832b8ca2651471a862e489e4f44f6754019e8c795a20a582e2f64a8ec13c7d@ec2-54-225-240-168.compute-1.amazonaws.com:5432/ddk942mhci1bsc";
 const massiveInstance = massive.connectSync({connectionString: connectionString});
-const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -19,3 +19,5 @@ app.use('/', index);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port ', app.get('port'));
 });
+
+module.exports = {app: app, server: server};
