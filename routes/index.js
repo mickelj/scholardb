c4b357ab690599cc286c06a697554e8a744259e8
Jason T. Mickel <mickelj@wlu.edu>
2017-04-26 14:49:27 -0400

    adding recent works

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+function getRandomScholars(req, res, next) {
   var db = req.app.get('db');
-  var nconf = req.app.get('nconf');
   db.run("SELECT last_name, lower(left(email, strpos(email, '@') - 1)) as image, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name, email HAVING count(works.id) > 2 ORDER BY random() LIMIT 18", function(err, results) {
-    res.render('index', {
-      title: nconf.get('application:appname') + " - Home",
-      message: "Welcome to ScholarsDB!",
-      tagline: nconf.get('application:tagline'),
-      people: results,
-      logo: nconf.get('application:logo'),
-      appname: nconf.get('application:appname'),
-      sampling: "A Sample of " + nconf.get('application:orgshortname') + " Scholars",
-      defimgext: nconf.get('application:defimgext'),
-      imgrootdir: nconf.get('application:imgrootdir')
-    });
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.scholars = results;
+    return next();
   });
-});
+}
+
+function getRecentWorks(req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT works.id, type, title_primary, contributors, publication_date_year, name FROM works JOIN publications ON publications.id = works.publication_id ORDER BY works.updated_at DESC LIMIT 3;", function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.works = results;
+    next();
+  });
+}
+
+function renderHomePage(req, res) {
+  var nconf = req.app.get('nconf');
+  res.render('index', {
+    people: req.scholars,
+    works: req.works,
+    title: nconf.get('application:appname') + " - Home",
+    message: "Welcome to ScholarsDB!",
+    tagline: nconf.get('application:tagline'),
+    logo: nconf.get('application:logo'),
+    appname: nconf.get('application:appname'),
+    sampling: "A Sample of " + nconf.get('application:orgshortname') + " Scholars",
+    defimgext: nconf.get('application:defimgext'),
+    imgrootdir: nconf.get('application:imgrootdir'),
+    organization: nconf.get('application:organization')
+  });
+}
+
+router.get('/', getRandomScholars, getRecentWorks, renderHomePage);
 
 module.exports = router;
