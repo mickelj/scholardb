fb446c2f05f2acec8faab031b2ec3f7cccc399de
Jason T. Mickel <mickelj@wlu.edu>
2017-04-26 10:18:51 -0400

    fixed app export error?

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,12 @@
-const express = require('express');
-const router = express.Router();
+const app = require('../index');
+//const router = express.Router();
+const db = app.get('db');
 
-var Routes = function(app) {
-  const db = app.get('db');
-
-  console.log(app);
-  app.get('/', function(req, res, next) {
-    //db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
-      //console.log(results);
-      res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
-    //});
+app.get('/', function(req, res, next) {
+  db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
+    console.log(results);
+    res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
   });
-}
+});
 
-module.exports = Routes;
+module.exports = router;
