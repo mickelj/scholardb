dc3d679a0005a0537eb3cace7b9bf83badeab4ad
Jason T. Mickel <mickelj@wlu.edu>
2017-04-26 10:00:23 -0400

    another fix attempt on db and routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = require('../index.js');
 const router = express.Router();
-const db = app.get('db');
 
-router.get('/', function(req, res, next) {
-  db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
-    console.log(results);
-    res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
+var Routes = function(app) {
+  const db = app.get('db');
+
+  router.get('/', function(req, res, next) {
+    db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
+      console.log(results);
+      res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
+    });
   });
-});
+}
 
-module.exports = router;
+module.exports = Routes;
