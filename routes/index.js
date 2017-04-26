85f3e9369cac83e709e1db29976f3001bb49c166
Jason T. Mickel <mickelj@wlu.edu>
2017-04-26 09:44:06 -0400

    Testing db connection

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const db = app.get('db');
 
 router.get('/', function(req, res, next) {
-  res.render('index', {title: "ScholarsDB - Home", "message": "Welcome to ScholarsDB!"});
+  db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
+    console.log(results);
+    res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
+  });
 });
 
 module.exports = router;
