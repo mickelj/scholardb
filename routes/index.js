6cc491e7ef8682c02841c675ec6a456a91f80bb9
Jason T. Mickel <mickelj@wlu.edu>
2017-04-26 10:37:53 -0400

    using req to get app?

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var app = express();
 var router = express.Router();
-var db = app.get('db');
 
 router.get('/', function(req, res, next) {
+  var db = req.app.get('db');
   db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
     console.log(results);
     res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
