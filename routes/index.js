20d8694b24874d5b39dcdd2a67e24fb17748c1af
Jason T. Mickel <mickelj@wlu.edu>
2017-04-26 10:07:04 -0400

    fix 3

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 var Routes = function(app) {
   const db = app.get('db');
 
-  router.get('/', function(req, res, next) {
+  app.get('/', function(req, res, next) {
     db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
       console.log(results);
       res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
