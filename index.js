60f5cd3cec27e15d773958a65688d3bb082d8e0d
Jason T. Mickel <mickelj@wlu.edu>
2017-04-26 10:24:43 -0400

    and again...

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-const bodyparser = require('body-parser');
-const index = require('./routes/index');
-const massive = require('massive');
-const connectionString = "postgres://knyadlimluugnb:ab832b8ca2651471a862e489e4f44f6754019e8c795a20a582e2f64a8ec13c7d@ec2-54-225-240-168.compute-1.amazonaws.com:5432/ddk942mhci1bsc";
-const massiveInstance = massive.connectSync({connectionString: connectionString});
+var bodyparser = require('body-parser');
+var index = require('./routes/index');
+var massive = require('massive');
+var connectionString = "postgres://knyadlimluugnb:ab832b8ca2651471a862e489e4f44f6754019e8c795a20a582e2f64a8ec13c7d@ec2-54-225-240-168.compute-1.amazonaws.com:5432/ddk942mhci1bsc";
+var massiveInstance = massive.connectSync({connectionString: connectionString});
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const app = require('../index');
-//const router = express.Router();
-const db = app.get('db');
+var app = require('../index');
+var router = express.Router();
+var db = app.get('db');
 
-app.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   db.run("SELECT last_name, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id GROUP BY last_name HAVING count(works.id) > 2 ORDER BY random() LIMIT 16", function(err, results) {
     console.log(results);
     res.render('index', {title: "ScholarsDB - Home", message: "Welcome to ScholarsDB!"});
