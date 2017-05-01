e2a692377ed3a888f2fee8fff2319c5f709b3dba
Jason T. Mickel <mickelj@wlu.edu>
2017-05-01 15:50:35 -0400

    fixed route error

--- routes/works.js
+++ routes/works.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/works', function(req, res) {
+router.get('/', function(req, res) {
   res.render('works', {
     title: nconf.get('application:appname') + " - Works",
     tagline: nconf.get('application:tagline'),
