74e65aa5eeb7bb6bc77b6a5eb5750584aab0fbec
Jason T. Mickel <mickelj@wlu.edu>
2017-05-03 14:39:08 -0400

    Implementing works filters

--- routes/works.js
+++ routes/works.js
@@ -1,7 +1,79 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res) {
+function getWorkTypeCount (req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT description, COUNT(type) FROM works JOIN work_types USING (type) GROUP BY description ORDER BY count(description) DESC", function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.filter_worktypes = results;
+    return next();
+  });
+}
+
+function getDeptWorkCount (req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT groups.name, COUNT(works.id) FROM works, JSONB_TO_RECORDSET(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id, groups JOIN UNNEST(p.group_membership) AS group_id ON group_id = groups.id WHERE groups.hidden = false GROUP BY groups.name ORDER BY COUNT(works.id) DESC", function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.filter_deptworks = results;
+    return next();
+  });
+}
+
+function getPeopleWorkCount (req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT CONCAT_WS(', ', last_name, first_name) AS full_name, COUNT(works.id) FROM works, JSONB_TO_RECORDSET(works.contributors) AS w(person_id int) JOIN people p on p.id = person_id GROUP BY full_name ORDER BY COUNT(works.id) DESC;", function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.filter_peopleworks = results;
+    return next();
+  });
+}
+
+function getYearWorkCount (req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT publication_date_year, COUNT(id) FROM works GROUP BY publication_date_year ORDER BY publication_date_year DESC", function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.filter_yearworks = results;
+    return next();
+  });
+}
+
+function getPublicationWorkCount (req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT name, COUNT(works.id) FROM works JOIN publications ON publications.id = works.publication_id WHERE name <> 'Unknown' GROUP BY name ORDER BY COUNT(works.id) DESC", function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.filter_publicationworks = results;
+    return next();
+  });
+}
+
+function getPublisherWorkCount (req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT publishers.name, COUNT(works.id) FROM works JOIN publications ON publications.id = works.publication_id JOIN publishers ON publishers.id = publications.publisher_id WHERE publishers.name <> 'Unknown' AND publications.name <> 'Unknown' GROUP BY publishers.name ORDER BY COUNT(works.id) DESC", function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.filter_publisherworks = results;
+    return next();
+  });
+}
+
+function renderWorksList (req, res) {
   var nconf = req.app.get('nconf');
   res.render('works', {
     title: nconf.get('application:appname') + " - Works",
@@ -12,8 +84,15 @@ router.get('/', function(req, res) {
     defimgext: nconf.get('application:defimgext'),
     imgrootdir: nconf.get('application:imgrootdir'),
     organization: nconf.get('application:organization'),
-    searchdeftext: nconf.get('application:searchdeftext')
-  })
-});
+    searchdeftext: nconf.get('application:searchdeftext'),
+    filter_worktypes: req.filter_worktypes,
+    filter_deptworks: req.filter_deptworks,
+    filter_peopleworks: req.filter_peopleworks,
+    filter_publicationworks: req.filter_publicationworks,
+    filter_publisherworks: req.filter_publisherworks
+  });
+}
+
+router.get('/', getWorkTypeCount, getDeptWorkCount, getPeopleWorkCount, getPublicationWorkCount, getPublisherWorkCount, renderWorksList);
 
 module.exports = router;
