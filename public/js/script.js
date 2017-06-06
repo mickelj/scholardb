78da60266cfa751aaad1ab4beb3c5c7332af59ed
Jason T. Mickel <mickelj@wlu.edu>
2017-06-06 13:54:09 -0400

    More filter play

--- public/js/script.js
+++ public/js/script.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
 
    $(".filter-toggle").on('click', function() {
      $(this).nextAll().toggleClass('hide');
+     var txt = $(this).next().is(':visible') ? 'Show fewer results...' : 'Show more results...';
+     $(this).text(txt);
    });
 });
