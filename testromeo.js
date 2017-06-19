690babfa322f51feca1c0660bc75fad04ef96d2f
Jason T. Mickel <mickelj@wlu.edu>
2017-06-19 14:39:05 -0400

    found the offending list

--- testromeo.js
+++ testromeo.js
@@ -14,8 +14,8 @@ request.get(romeourl + '&issn=1943-345X', function(err, res, body) {
       return next(err);
     }
 
-    if (result.romeoapi.publishers[0].publisher[0].postprints[0].postarchiving[0]) {
-      console.log(util.inspect(result.romeoapi.publishers[0].publisher[0].postprints[0].postarchiving[0], false, null));
+    if (result.romeoapi.publishers[0].publisher[0].copyrightlinks[0]) {
+      console.log(util.inspect(result.romeoapi.publishers[0].publisher[0].copyrightlinks[0], false, null));
     } else {
       console.log('None');
     }
