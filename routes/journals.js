3b24035bef9d05978cc31b85e7cc18fbb55faebf
Jason T. Mickel <mickelj@wlu.edu>
2017-06-19 10:54:05 -0400

    adding journal detail page

--- routes/journals.js
+++ routes/journals.js
@@ -31,7 +31,6 @@ function getJournalWorkCount (req, res, next) {
   });
 }
 
-
 function getLetterPagerCounts (req, res, next) {
   var db = req.app.get('db');
 
@@ -63,7 +62,88 @@ function renderJournalList(req, res) {
   });
 }
 
+function getJournalDetail (req, res, next) {
+  var db = req.app.get('db');
+  var journal_id = req.params.id;
+
+  db.run("SELECT j.id, j.name, publisher_id, p.name as publisher_name, identifiers FROM publications j LEFT JOIN publishers p ON j.publisher_id = p.id WHERE j.id = $1", [journal_id], function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.journal_detail = results[0];
+    return next();
+  });
+}
+
+function getJournalPeople (req, res, next) {
+  var db = req.app.get('db');
+  var journal_id = req.params.id;
+
+  db.run("SELECT person_id, first_name, last_name, lower(left(email, strpos(email, '@') - 1)) as image, count(works.id) FROM works, jsonb_to_recordset(works.contributors) AS w(person_id int) LEFT JOIN people p ON p.id = person_id WHERE publication_id = $1 AND active = true GROUP BY person_id, first_name, last_name, email ORDER BY last_name, first_name", [journal_id], function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.journal_people = results;
+    return next();
+  });
+}
+
+function getJournalAllWorkCount (req, res, next) {
+  var db = req.app.get('db');
+  var journal_id = req.params.id;
+
+  db.run("SELECT j.id, COUNT(works.id) AS cnt FROM publications j JOIN works ON j.id = works.publication_id WHERE j.id = $1 GROUP BY j.id", [journal_id], function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.total_works = results[0].cnt;
+    return next();
+  });
+}
+
+function getJournalWorksList (req, res, next) {
+  var db = req.app.get('db');
+  var journal_id = req.params.id;
+  var limit = req.query.limit ? req.query.limit : 10;
+  var offset = req.query.page ? (req.query.page - 1) * limit : 0;
+
+  db.run("SELECT DISTINCT works.id, title_primary as work_title, description as work_type, contributors, publications.name as publication, publications.authority_id as pubid, publication_date_year as year FROM works JOIN publications ON publications.authority_id = works.publication_id JOIN work_types USING (type), JSONB_TO_RECORDSET(works.contributors) AS w(person_id int) LEFT JOIN people p ON person_id = p.id WHERE publications.id = $1 ORDER BY publication_date_year DESC, works.id DESC LIMIT $2 OFFSET $3", [journal_id, limit, offset], function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.journal_works_list = results;
+    req.works_count = results.length;
+    return next();
+  });
+}
+
+function renderJournalDetail(req, res) {
+  var nconf = req.app.get('nconf');
+  var limit = req.query.limit ? req.query.limit : 10;
+  var page_count = Math.ceil(req.total_works / limit);
+  var cur_page = req.query.page ? req.query.page : 1;
+  var offset = (cur_page - 1) * limit;
+
+  res.render('journal_detail', {
+    appconf: nconf.get('application'),
+    title: nconf.get('application:appname') + " - Journal: " + req.journal_detail.name,
+    journal: req.journal_detail,
+    people: req.journal_people,
+    works_list: req.journal_works_list,
+    total_works: req.total_works,
+    limit: limit,
+    page_count: page_count,
+    cur_page: cur_page,
+    offset: offset,
+    cur_list: req.baseUrl + "/" + req.params.id
+  });
+}
+
 router.get('/', getJournalList, getJournalWorkCount, getLetterPagerCounts, renderJournalList);
-//router.get('/:id', getDeptDetail, getDeptPeople, getDeptWorksCount, getDeptWorksList, getWorksImages, renderDeptDetail);
+router.get('/:id', getJournalDetail, getJournalPeople, getJournalAllWorkCount, getJournalWorksList, renderJournalDetail);
 
 module.exports = router;
