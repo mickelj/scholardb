cffd6a6c63d158fd410f058f3bc85be336560392
Jason T. Mickel <mickelj@wlu.edu>
2017-06-23 10:52:48 -0400

    try #3

--- public/js/script.js
+++ public/js/script.js
@@ -46,11 +46,12 @@ $(document).ready(function() {
   $(".detail-works").css('max-height', ($(".detail-profile-sidebar").height()));
 
   $(".filter-group .collection-item").on('click', function() {
-    var filters = JSON.parse(getUrlParameter('filters'));
-    var filtype = $(this).data('filter-type');
-    var val = $(this).data('filter-id');
+    var filstr = getUrlParameter('filters');
+    if (filstr) {
+      var filters = JSON.parse(filstr);
+      var filtype = $(this).data('filter-type');
+      var val = $(this).data('filter-id');
 
-    if (filters) {
       var filindex = _.indexOf(_.findWhere(filters, {type: filtype}));
       if (filindex > -1) {
         if (!(val in filters[filindex].ids)) {
