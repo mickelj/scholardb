cbe2bb9ea33ca8d1bb7d92ee86e12c28f52337ea
Jason T. Mickel <mickelj@wlu.edu>
2017-06-23 12:06:19 -0400

    added filter types

--- routes/works.js
+++ routes/works.js
@@ -75,18 +75,6 @@ function getPublisherWorkCount (req, res, next) {
   });
 }
 
-function getWorksCount(req, res, next) {
-  var db = req.app.get('db');
-  db.run("SELECT count(*) as total_works FROM works", function(err, results) {
-    if (err || !results.length) {
-      return next(err);
-    }
-
-    req.total_works = results[0].total_works;
-    return next();
-  });
-}
-
 function getWorksList(req, res, next) {
   var db = req.app.get('db');
   var limit = req.query.limit ? req.query.limit : 10;
@@ -94,8 +82,8 @@ function getWorksList(req, res, next) {
   var filters = req.query.filters ? JSON.parse(req.query.filters) : null;
   var filterlist = [];
 
-  if (_.findWhere(filters, {type: 'worktype'})) {
-    filterlist.push("works.type IN ('" + _.findWhere(filters, {type: 'worktype'}).ids.join("','") + "')");
+  if (_.findWhere(filters, {type: 'worktypes'})) {
+    filterlist.push("works.type IN ('" + _.findWhere(filters, {type: 'worktypes'}).ids.join("','") + "')");
   }
 
   if (_.findWhere(filters, {type: 'people'})) {
@@ -108,8 +96,8 @@ function getWorksList(req, res, next) {
     filterlist.push(people);
   }
 
-  if (_.findWhere(filters, {type: 'year'})) {
-    filterlist.push("works.publication_date_year IN (" + _.findWhere(filters, {type: 'year'}).ids.toString() + ")");
+  if (_.findWhere(filters, {type: 'years'})) {
+    filterlist.push("works.publication_date_year IN (" + _.findWhere(filters, {type: 'years'}).ids.toString() + ")");
   }
 
   if (_.findWhere(filters, {type: 'publications'})) {
@@ -120,14 +108,26 @@ function getWorksList(req, res, next) {
     filterlist.push("publishers.id IN (" + _.findWhere(filters, {type: 'publishers'}).ids.toString() + ")");
   }
 
-  filters = (filterlist.length) ? filterlist.join(" AND ") : "TRUE";
+  req.filters = (filterlist.length) ? filterlist.join(" AND ") : "TRUE";
 
-  db.run("SELECT works.id, title_primary as work_title, title_secondary, title_tertiary, description as work_type, contributors, publications.name as publication, publications.id as pubid, pi.identifier, publication_date_year as year FROM works LEFT JOIN publications ON publications.id = works.publication_id LEFT JOIN publishers ON publications.publisher_id = publishers.id LEFT JOIN work_types USING (type) LEFT JOIN LATERAL (select identifier from publications p2, JSONB_TO_RECORDSET(identifiers) as w(type text, identifier text) WHERE p2.id = publications.id AND type LIKE 'ISBN%') pi ON TRUE WHERE " + filters + " ORDER BY publication_date_year DESC, works.id DESC LIMIT $1 OFFSET $2", [limit, offset], function(err, results) {
+  db.run("SELECT DISTINCT works.id, title_primary as work_title, title_secondary, title_tertiary, description as work_type, contributors, publications.name as publication, publications.id as pubid, pi.identifier, publication_date_year as year FROM works LEFT JOIN publications ON publications.id = works.publication_id LEFT JOIN publishers ON publications.publisher_id = publishers.id LEFT JOIN work_types USING (type) LEFT JOIN LATERAL (select identifier from publications p2, JSONB_TO_RECORDSET(identifiers) as w(type text, identifier text) WHERE p2.id = publications.id AND type LIKE 'ISBN%' LIMIT 1) pi ON TRUE WHERE " + req.filters + " ORDER BY publication_date_year DESC, works.id DESC LIMIT $1 OFFSET $2", [limit, offset], function(err, results) {
     if (err || !results.length) {
       return next(err);
     }
 
     req.works_list = results;
+    return next();
+  });
+}
+
+function getWorksCount(req, res, next) {
+  var db = req.app.get('db');
+  db.run("SELECT COUNT(works.id) as total_works FROM works LEFT JOIN publications ON publications.id = works.publication_id LEFT JOIN publishers ON publications.publisher_id = publishers.id LEFT JOIN work_types USING (type) LEFT JOIN LATERAL (select identifier from publications p2, JSONB_TO_RECORDSET(identifiers) as w(type text, identifier text) WHERE p2.id = publications.id AND type LIKE 'ISBN%' LIMIT 1) pi ON TRUE WHERE " + req.filters, function(err, results) {
+    if (err || !results.length) {
+      return next(err);
+    }
+
+    req.total_works = results[0].total_works;
     return next();
   });
 }
@@ -186,7 +186,7 @@ function getWorkDetail(req, res, next) {
   var db = req.app.get('db');
   var work_id = req.params.id;
 
-  db.run("SELECT works.id, title_primary as work_title, title_secondary, title_tertiary, description as work_type, contributors, publications.name as publication, publications.authority_id as pubid, publishers.name as publisher, publishers.id as publisherid, publication_date_year as year, volume, issue, start_page, end_page, location, works.url, pi.identifier FROM works LEFT JOIN publications ON publications.id = works.publication_id LEFT JOIN work_types USING (type) LEFT JOIN publishers ON publishers.id = publications.publisher_id LEFT JOIN LATERAL (select identifier from publications p2, JSONB_TO_RECORDSET(identifiers) as w(type text, identifier text) WHERE p2.id = publications.id AND type LIKE 'ISBN%') pi ON TRUE WHERE works.id = $1", [work_id], function(err, results) {
+  db.run("SELECT works.id, title_primary as work_title, title_secondary, title_tertiary, description as work_type, contributors, publications.name as publication, publications.authority_id as pubid, publishers.name as publisher, publishers.id as publisherid, publication_date_year as year, volume, issue, start_page, end_page, location, works.url, pi.identifier FROM works LEFT JOIN publications ON publications.id = works.publication_id LEFT JOIN work_types USING (type) LEFT JOIN publishers ON publishers.id = publications.publisher_id LEFT JOIN LATERAL (select identifier from publications p2, JSONB_TO_RECORDSET(identifiers) as w(type text, identifier text) WHERE p2.id = publications.id AND type LIKE 'ISBN%' LIMIT 1) pi ON TRUE WHERE works.id = $1", [work_id], function(err, results) {
     if (err || !results.length) {
       return next(err);
     }
@@ -234,7 +234,7 @@ function renderWorkDetail(req, res) {
   });
 }
 
-router.get('/', getWorkTypeCount, getDeptWorkCount, getPeopleWorkCount, getYearWorkCount, getPublicationWorkCount, getPublisherWorkCount, getWorksCount, getWorksList, getWorksImages, renderWorksList);
+router.get('/', getWorkTypeCount, getDeptWorkCount, getPeopleWorkCount, getYearWorkCount, getPublicationWorkCount, getPublisherWorkCount, getWorksList, getWorksCount, getWorksImages, renderWorksList);
 
 router.get('/:id', getWorkDetail, getSingleImage, renderWorkDetail);
 
