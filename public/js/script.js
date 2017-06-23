bbb7bc5ac6bc4820fd45ac34e6a69e9c4d5bca75
Jason T. Mickel <mickelj@wlu.edu>
2017-06-23 10:47:30 -0400

    error when filters isn't there

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,19 @@
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
     }
-};
+
+    return undefined;
+  }
+}
 
 $(document).ready(function() {
   $(".button-collapse").sideNav();
