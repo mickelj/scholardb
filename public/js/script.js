76522957cc5d4d1211e3b20fa332dfa4e3e33c3c
Jason T. Mickel <mickelj@wlu.edu>
2017-06-23 15:05:22 -0400

    trying the delete filter option

--- public/js/script.js
+++ public/js/script.js
@@ -48,6 +48,8 @@ $(document).ready(function() {
   $(".filter-group .collection-item").on('click', function() {
     var filtype = $(this).data('filter-type');
     var val = $(this).data('filter-id');
+    var limit = getUrlParameter('limit') ? "&limit=" + getUrlParameter('limit') : "";
+    var page = getUrlParameter('page') ? "&page=" + getUrlParameter('page') : "";
 
     var filstr = getUrlParameter('filters');
     if (filstr) {
@@ -65,6 +67,32 @@ $(document).ready(function() {
       var filters = [{type: filtype, ids: [val]}];
     }
 
-    window.location.href = "/works?filters=" + JSON.stringify(filters);
+    window.location.href = "/works?filters=" + JSON.stringify(filters) + limit + page;
+  });
+
+  $(".filter-group .chip .close").on('click', function() {
+    var filtype = $(this).parent().data('filter-type');
+    var val = $(this).parent().data('filter-id');
+    var limit = getUrlParameter('limit') ? "&limit=" + getUrlParameter('limit') : "";
+    var page = getUrlParameter('page') ? "&page=" + getUrlParameter('page') : "";
+    var filstr = getUrlParameter('filters');
+    var filters = JSON.parse(filstr);
+
+    var filindex = _.indexOf(filters, _.findWhere(filters, {type: filtype}));
+    if (filindex > -1) {
+      if (val in filters[filindex].ids) {
+        if (filter[filindex].ids.length > 1) {
+          filters[filindex].ids.splice($.inArray(val, filters[filindex].ids), 1);
+        } else { // remove the entire object from the array
+          filters.splice(filindex, 1);
+        }
+      }
+    }
+
+    if (filters.length) {
+      window.location.href = "/works?filters=" + JSON.stringify(filters) + limit + page;
+    } else {
+      window.location.href = "/works";
+    }
   });
 });
