7daf221d9f75c9fcf95e94e25393258ce3751f64
Jason T. Mickel <mickelj@wlu.edu>
2017-06-28 13:25:34 -0400

    fix css error

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ nconf.file('database', 'config/database.json');
 const connectionString = (process.env.DATABASE_URL || nconf.get('database:connectionString'));
 const massiveInstance = massive.connectSync({connectionString: connectionString});
 app.set('port', (process.env.PORT || 5000));
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.use('/materialize', express.static(__dirname + '/node_modules/materialize-css/dist'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
