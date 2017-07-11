7f3440067369425d2caa9af5b7f5cd3305de4789
Jason T. Mickel <mickelj@wlu.edu>
2017-07-11 10:41:36 -0400

    hmmm

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const app = express();
 
-module.exports = {
-  logErrors: function(err, req, res, next) {
-    console.error(err.stack);
+module.exports.logErrors = function(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+};
+
+module.exports.clientErrorHandler = function(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Error occurred'});
+  } else {
     next(err);
-  },
-
-  clientErrorHandler: function(err, req, res, next) {
-    if (req.xhr) {
-      res.status(500).send({ error: 'Error occurred'});
-    } else {
-      next(err);
-    }
-  },
-
-  errorHandler: function(err, req, res, next) {
-    res.status(500);
-    res.render('error', {error: err});
   }
-}
+};
+
+module.exports.errorHandler = function(err, req, res, next) {
+  res.status(500);
+  res.render('error', {error: err});
+};
