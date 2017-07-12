b5cefb2e05e6bd7394aa22f2469839e1bdfe3145
Jason T. Mickel <mickelj@wlu.edu>
2017-07-12 16:31:16 -0400

    one more debug

--- routes/auth.js
+++ routes/auth.js
@@ -12,12 +12,12 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', authHelpers.loginRedirect, (req, res, next) => {
-  console.log('made it to the post');
   passport.authenticate('local', (err, user, info) => {
     if (err) res.status(500).send('Error in passort local authentication module');
     if (!user) res.redirect('/auth/login');
     if (user) {
-      req.login(user, function(err) {
+      req.logIn(user, function(err) {
+        console.log('made it to the login');
         if (err) res.status(500).send('Error logging in');
         req.session.save( (err) => {
           if (err) return next(err);
