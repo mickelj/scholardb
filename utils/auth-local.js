b2289c7cae1d433b5c1fa21b5ef03ce66d37cdc2
Jason T. Mickel <mickelj@wlu.edu>
2017-07-13 13:45:10 -0400

    style + prevent users w/o password from loggin in

--- utils/auth-local.js
+++ utils/auth-local.js
@@ -19,11 +19,11 @@ passport.deserializeUser((id, done) => {
 
 passport.use(new LocalStrategy(options, (username, password, done) => {
   //check to see if the username exists
-  db.run('SELECT * FROM people WHERE email = $1', [username], function(err, results) {
+  db.run("SELECT * FROM people WHERE email = $1 AND password IS NOT NULL AND password <> ''", [username], function(err, results) {
     var user = results[0];
     if (err) return done(err);
-    if (!results.length) return done(null, false, {message: 'Username not found'});
-    if (user.password && !authHelpers.comparePass(password, user.password)) {
+    if (!results.length) return done(null, false, {message: 'User account not found'});
+    if (!authHelpers.comparePass(password, user.password)) {
       return done(null, false, {message: 'Incorrect password'});
     } else {
       return done(null, user);
