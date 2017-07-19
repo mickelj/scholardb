355c56f69cc796dd4ec22f7840e00173d7c1a14c
Mickel <mickelj@wlu.edu>
2017-07-19 12:40:41 -0400

    updated pen names route

--- routes/user.js
+++ routes/user.js
@@ -13,17 +13,36 @@ function getPenNames(req, res, next) {
   });
 }
 
-function savePenNames(req, res) {
+function checkPenName(req, res, next) {
+  var req.pn = req.body.last_name + ", " + req.body.first_name + (req.body.middle_name ? " " + req.body.middle_name : "");
+  var req.mn = gn.genMachineName(pn);
+
+  db.run("SELECT id FROM pennames WHERE machine_name ILIKE $1", ['%' + mn + '%'], (err, results) => {
+    if (err) return next(err);
+    if (results.length) {
+      req.dberr = 'Pen name is already in database.';
+      return next();
+    }
+    return next();
+  })
+}
+
+function savePenName(req, res) {
   req.session.flash = [];
 
-  if (!req.body.penname) {
+  if (req.dberr) {
+    req.flash('error', req.dberr);
+    res.redirect('/user/penname');
+  }
+
+  if (!req.pn) {
     req.flash('error', 'Pen name was empty');
     res.redirect('/user/penname');
   }
 
   db.pennames.insert({people_id: req.user.id, display_name: req.body.penname, machine_name: gn.genMachineName(req.body.penname)}, (err, results) => {
     if (err) {
-      req.flash('error', 'Error adding pen name to database');
+      req.flash('error', 'Error adding pen name to database: ' + err);
       res.redirect('/user/penname');
     }
     req.flash('success', 'Pen name added successfully');
@@ -53,6 +72,6 @@ router.get('/penname', authHelpers.loginRequired, getPenNames, (req, res, next) 
   });
 });
 
-router.post('/penname', authHelpers.loginRequired, savePenNames);
+router.post('/penname', authHelpers.loginRequired, checkPenName, savePenName);
 
 module.exports = router;
