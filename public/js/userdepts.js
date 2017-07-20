588c1095870d672d02a01e8da416dc02a82d9d7c
Mickel <mickelj@wlu.edu>
2017-07-20 09:58:01 -0400

    Remove tooltips because they stick after deleting

--- public/js/userdepts.js
+++ public/js/userdepts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var seltext = $("#deptlist option:selected").text();
     var selval = $("#deptlist").val();
 
-    $("#newdeptlist .collection").append("<li class='collection-item'><div>" + seltext + " <a class='secondary-content tooltipped red-text text-darken-4 deldept' href='#!' data-position='bottom' data-delay='50' data-tooltip='Remove " + seltext + "' data-deptid='" + selval + "' data-deptname='" + seltext + "'><i class='material-icons'>remove_circle</i></a></div></li>");
+    $("#newdeptlist .collection").append("<li class='collection-item'><div>" + seltext + " <a class='secondary-content red-text text-darken-4 deldept' href='#!' data-deptid='" + selval + "' data-deptname='" + seltext + "'><i class='material-icons'>remove_circle</i></a></div></li>");
 
     $("#newdeptheader").removeClass('hide');
     $("#newdeptlist").removeClass('hide');
