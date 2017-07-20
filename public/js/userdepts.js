6efd016d20b516c307507d8536e8aa87846bd4ce
Mickel <mickelj@wlu.edu>
2017-07-20 09:55:45 -0400

    fix how jquery gets sel text

--- public/js/userdepts.js
+++ public/js/userdepts.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
   $("#deptlist").on('change', function() {
-    $("#newdeptlist .collection").append("<li class='collection-item'><div>" + $(this).text() + " <a class='secondary-content tooltipped red-text text-darken-4 deldept' href='#!' data-position='bottom' data-delay='50' data-tooltip='Remove " + $(this).text() + "' data-deptid='" + $(this).val() + "' data-deptname='" + $(this).text() + "'><i class='material-icons'>remove_circle</i></a></div></li>");
+    var seltext = $("#deptlist option:selected").text();
+    var selval = $("#deptlist").val();
+
+    $("#newdeptlist .collection").append("<li class='collection-item'><div>" + seltext + " <a class='secondary-content tooltipped red-text text-darken-4 deldept' href='#!' data-position='bottom' data-delay='50' data-tooltip='Remove " + seltext + "' data-deptid='" + selval + "' data-deptname='" + seltext + "'><i class='material-icons'>remove_circle</i></a></div></li>");
 
     $("#newdeptheader").removeClass('hide');
     $("#newdeptlist").removeClass('hide');
     var indepts = $("#inDepts").val();
     if (indepts) {
-      $("#inDepts").val(indepts + ",{id: " + $(this).val() + ", name: " + $(this).text() + "}");
+      $("#inDepts").val(indepts + ",{id: " + selval + ", name: " + seltext + "}");
     } else {
-      $("#inDepts").val("{id: " + $(this).val() + ", name: " + $(this).text() + "}");
+      $("#inDepts").val("{id: " + selval + ", name: " + seltext + "}");
     }
 
     // REMOVE THIS FROM outDepts IF FOUND THERE
