eac4ab846c8dd0268db54fde372c1eab14fc7da4
Mickel <mickelj@wlu.edu>
2017-07-24 14:55:20 -0400

    debug 7

--- public/js/userdepts.js
+++ public/js/userdepts.js
@@ -2,7 +2,7 @@ function removeFromArray(item, arrayToEdit) {
   var arr = JSON.parse("[" + JSON.stringify(arrayToEdit.value) + "]");
   var index = _.indexOf(arrayToEdit, _.findWhere(arr, item));
   if (index) arr.splice(index, 1);
-  var arrstr = JSON.stringify(arr).substring(1, arr.length-1);
+  var arrstr = JSON.stringify(arr);
   arrayToEdit.value = arrstr;
 }
 
