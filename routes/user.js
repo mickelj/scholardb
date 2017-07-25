13c31a6ada58995e1558aaae60a830a41941d11b
Mickel <mickelj@wlu.edu>
2017-07-25 16:46:45 -0400

    debug 20

--- routes/user.js
+++ routes/user.js
@@ -102,6 +102,7 @@ function processPhoto(req, res, next) {
       var url = 'https://scholarsdb.omeka.wlu.edu/' + "upload.php";
       var r = request.post(url, (err, response, body) => {
         resp = JSON.parse(body);
+        console.log(resp);
         if (resp.err) {
           req.flash('error', 'Error saving photo: ' + resp.err);
           return res.redirect('/user/photo');
