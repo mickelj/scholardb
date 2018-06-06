90f642b5c88db64ed6ef7fb4afccf58ca10a3c14
Jason T. Mickel <mickelj@jtmimac.ad.wlu.edu>
2018-06-06 14:56:53 -0400

    more debugging

--- utils/convertZoteroOutput.js
+++ utils/convertZoteroOutput.js
@@ -21,42 +21,50 @@ module.exports = {
 		request(options, (err, response, csljsonConv) => {
 			// If the JSON won't convert to CSL, convert to BibTex and then CSL using another library
 			// Otherwise, all converted properly so send back the CSL-JSON
-			if (response && response.statusCode !== 200) {
-				//url = nconf.get('zotero:tsurl') + "/export?format=biblatex";
-				url = "https://scholarsdb-zotero.herokuapp.com/export?format=biblatex";
-				var options = {
-					headers: {
-						'Content-Type' : 'application/json'
-					},
-					uri: url,
-					method: 'POST',
-					body: zjson
-				};
-
-				request(options, (err, response, biblatexConv) => {
-					// Whomp whomp...wouldn't convert to return the error message
-					if (response && response.statusCode !== 200) return {err: response.statusCode, msg: biblatexConv};
-
-					var bloptions = {
-						format: 'string',
-						type: 'json',
-						style: 'csl',
-						lang: 'en-US'
+			if (response) {
+				if (response.statusCode !== 200) {
+					//url = nconf.get('zotero:tsurl') + "/export?format=biblatex";
+					url = "https://scholarsdb-zotero.herokuapp.com/export?format=biblatex";
+					var options = {
+						headers: {
+							'Content-Type' : 'application/json'
+						},
+						uri: url,
+						method: 'POST',
+						body: zjson
 					};
-					var data = new Cite(biblatexConv, bloptions);
 
+					request(options, (err, response, biblatexConv) => {
+						// Whomp whomp...wouldn't convert to return the error message
+						if (response) {
+							if (response.statusCode !== 200) return {err: response.statusCode, msg: biblatexConv};
+
+							var bloptions = {
+								format: 'string',
+								type: 'json',
+								style: 'csl',
+								lang: 'en-US'
+							};
+							var data = new Cite(biblatexConv, bloptions);
+
+							result = {
+								rcode: response.statusCode,
+								msg: data.get()
+							};
+							cb(result);
+						} else {
+							cb({rcode: 500, msg: "No response from Zotero server on biblatex -> csl"});
+						}
+					});
+				} else {
 					result = {
 						rcode: response.statusCode,
-						msg: data.get()
+						msg: csljsonConv
 					};
 					cb(result);
-				});
+				}
 			} else {
-				result = {
-					rcode: response.statusCode,
-					msg: csljsonConv
-				};
-				cb(result);
+				cb({rcode: 500, msg: "No response from Zotero server on zjson -> csl"});
 			}
 		});
 	}
