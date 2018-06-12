7d3e3be0078947346395684a3d073b378e835666
Jason T. Mickel <mickelj@jtmimac.ad.wlu.edu>
2018-06-12 11:16:31 -0400

    more admin prep

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const authHelpers = require('../utils/auth-helpers');
 const db = require('../utils/db');
 
-router.get('/', authHelpers.adminRequired, (req, res, next) => {
-  res.render('admin');
+router.get('/', authHelpers.adminRequired, (req, res) => {
+  var nconf = req.app.get('nconf');
+  
+  res.render('admin', {
+    appconf: nconf.get()
+  });
 });
 
 module.exports = router;
