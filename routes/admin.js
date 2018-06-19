07e70e41b0695cbb91ca72c00cb937e21dd60913
Jason T. Mickel <mickelj@jtmimac.ad.wlu.edu>
2018-06-19 12:39:36 -0400

    more debug

--- routes/admin.js
+++ routes/admin.js
@@ -15,10 +15,9 @@ function getADInfo(req, res) {
 
   AD.findUser(ADattributes, req.query.username, function(err, user) {
     if (err || !user) return res.json({});
-
+    console.log(JSON.stringify(user));
     if (req.query.adduser) {
       db.people.where("university_id = $1 OR email = $2", [user.university_id, user.mail], (err, results) => {
-        console.log(results);
         if (err) return res.json({});
         if (results) return res.json({exists: true});
         return res.json(JSON.stringify(user));
